4ce30e9e4a7eb537aeafa17c3b0f736f3442f21c
simdimlim <oldskooluv@hotmail.com>
2018-02-20 20:51:04 +1100

    implemented authentication and real time updates to db

--- ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
@@ -60,10 +60,20 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   }
 
   deinit {
+    if let refHandle = _refHandle {
+        self.ref.child("messages").removeObserver(withHandle: _refHandle)
+    }
   }
 
   func configureDatabase() {
-  }
+    ref = Database.database().reference()
+    // Listen for new messages in the Firebase database
+    _refHandle = self.ref.child("messages").observe(.childAdded, with: { [weak self] (snapshot) -> Void in
+    guard let strongSelf = self else { return }
+    strongSelf.messages.append(snapshot)
+    strongSelf.clientTable.insertRows(at: [IndexPath(row: strongSelf.messages.count-1, section: 0)], with: .automatic)
+        })
+    }
 
   func configureStorage() {
   }
@@ -106,7 +116,17 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     // Dequeue cell
     let cell = self.clientTable.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-
+    // Unpack message from Firebase DataSnapshot
+    let messageSnapshot = self.messages[indexPath.row]
+    guard let message = messageSnapshot.value as? [String: String] else { return cell }
+    let name = message[Constants.MessageFields.name] ?? ""
+    let text = message[Constants.MessageFields.text] ?? ""
+    cell.textLabel?.text = name + ": " + text
+    cell.imageView?.image = UIImage(named: "ic_account_circle")
+    if let photoURL = message[Constants.MessageFields.photoURL], let URL = URL(string: photoURL),
+        let data = try? Data(contentsOf: URL) {
+        cell.imageView?.image = UIImage(data: data)
+    }
     return cell
   }
 
@@ -121,6 +141,14 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   }
 
   func sendMessage(withData data: [String: String]) {
+    var mdata = data
+    mdata[Constants.MessageFields.name] = Auth.auth().currentUser?.displayName
+    if let photoURL = Auth.auth().currentUser?.photoURL {
+        mdata[Constants.MessageFields.photoURL] = photoURL.absoluteString
+    }
+    
+    // Push data to Firebase Database
+    self.ref.child("messages").childByAutoId().setValue(mdata)
   }
 
   // MARK: - Image Picker
@@ -163,7 +191,13 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   }
 
   @IBAction func signOut(_ sender: UIButton) {
-    dismiss(animated: true, completion: nil)
+    let firebaseAuth = Auth.auth()
+    do {
+        try firebaseAuth.signOut()
+        dismiss(animated: true, completion: nil)
+    } catch let signOutError as NSError {
+        print ("Error signing out: \(signOutError.localizedDescription)")
+    }
   }
 
   func showAlert(withTitle title: String, message: String) {
